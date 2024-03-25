9aa85ad1783c339a32c475738fa919fc1070dc9e
luisgomez7t <162279891+7tCam@users.noreply.github.com>
2024-03-25 22:47:26 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,9 +1,34 @@
 /* eslint-disable */
+//import remoteOriginUrl from "remote-origin-url";
 import "./style.css";
 
 window.onload = () => {
   //write your code here
-  let randomNumber = Math.random() * 10;
-  document.querySelector("#the-excuse").innerHTML = randomNumber;
-  console.log("Hello Rigo from the console! " + randomNumber);
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#the-excuse").innerHTML = generateExcuse();
+  });
+  console.log("Hello Rigo from the console! ");
+};
+let generateExcuse = () => {
+  let pronoun = ["A", "The"];
+  let subject = ["jogger", "rocoon", "dog", "driver", "comedian", "pincone"];
+  let action = ["took my", "threw my", "yelled at my", "stole my", "bit my"];
+  let possetion = ["homework", "toe", "car", "shoe"];
+  let where = ["on the street", "in my house", "in my driveway"];
+  let proIndx = Math.floor(Math.random() * pronoun.length);
+  let subjIndx = Math.floor(Math.random() * subject.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let possetionIndex = Math.floor(Math.random() * possetion.length);
+  let whereIndex = Math.floor(Math.random() * where.length);
+  return (
+    pronoun[proIndx] +
+    " " +
+    subject[subjIndx] +
+    " " +
+    action[actionIndex] +
+    " " +
+    possetion[possetionIndex] +
+    " " +
+    where[whereIndex]
+  );
 };
